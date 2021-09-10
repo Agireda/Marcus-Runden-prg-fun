fd8a73fb34ac10183cbbea2689e3ed124ca209d9
Marcus Rundén <marcus.runden@hotmail.com>
2021-09-10 16:35:43 +0200

    unity ignore

---
+++ Assets/assignment1.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class assignment1 : ProcessingLite.GP21
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  Line(4, 7, 4, 3);
+        //  Line(4, 5, 6, 5);
+        //  Line(6, 7, 6, 3);
+
+        //  Line(8, 5.5f, 8, 3);
+        //  Line(8, 7, 8, 6.8f);
+
+        Line(3, 3, 3, 12);
+        Line(3, 12, 6, 9);
+        Line(6, 9, 3, 6);
+        Line(3, 6, 6, 4);
+
+        Line(8, 3, 8, 10);
+        Line(8, 3, 11, 4);
+        Line(11, 4, 11, 10);
+        Line(8, 4, 11, 5);
+
+        Line(13, 3, 13, 10);
+        Line(13, 10, 15, 7);
+        Line(13, 7, 15, 3);
+        Line(15, 3, 15, 10);
+
+        Line(17, 3, 17, 10);
+        Line(17, 10, 19, 6);
+        Line(19, 6, 17, 3);
+
+        Line(20, 9, 22, 10);
+        Line(20, 9, 22, 7);
+        Line(22, 7, 20, 5);
+        Line(20, 5, 22, 3);
+        Line(21, 11, 22, 12);
+
+        Line(24, 3, 24, 10);
+        Line(24, 10, 26, 7);
+        Line(24, 7, 26, 3);
+        Line(26, 3, 26, 10);
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Marcus-Runden-prg-fun
+ 
